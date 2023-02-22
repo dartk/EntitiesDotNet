19ecc8030282640bbda3905362f231dd9c4805a7
dartk <dartk@users.noreply.github.com>
2023-02-22 14:55:05 +0700

    Adds inlining support for ForEach extensions

---
+++ EntitiesDotNet.Tests/Inlining/InliningTests.cs
@@ -0,0 +1,23 @@
+﻿using CSharp.SourceGen.Inlining.Attributes;
+using EntitiesDotNet;
+
+
+namespace EntityComponentSystem.Tests.Inlining;
+
+
+public partial class InliningTests
+{
+    [GenerateInlined(nameof(ForEachArrays_Inlined))]
+    public void ForEachArrays(EntityArrays arrays)
+    {
+        arrays.ForEach([Inline](int index, ref string s, ref double d) => { s = d.ToString(); });
+        arrays.ForEach([Inline](ref string s, ref double d) => { s = d.ToString(); });
+    }
+    
+    [GenerateInlined(nameof(ForEach_Inlined))]
+    public void ForEach(IComponentArray array)
+    {
+        array.ForEach([Inline](int index, ref string s, ref double d) => { s = d.ToString(); });
+        array.ForEach([Inline](ref string s, ref double d) => { s = d.ToString(); });
+    }
+}
